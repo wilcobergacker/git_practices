1d83ade844a68f727f04c6cc73c2dc81eca12ac2
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-15 21:06:14 +0100

    updated

--- class score.py
+++ class score.py
@@ -27,3 +27,14 @@ pieter.add_grade(Grade(55))
 
 print(pieter.grades)
 print(pieter.name)
+
+
+class User:
+  def __init__(self, name):
+    self.name = name
+
+  def __repr__(self):
+    return "Hiya {}!".format(self.name)
+
+devorah = User("Devorah")
+print(devorah)
