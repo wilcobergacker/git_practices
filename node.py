cd72196a5f33ab5acdd12d052e13a779d56b6221
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-17 22:00:02 +0100

    add Node information

---
+++ node.py
@@ -0,0 +1,154 @@
+# We'll be using our Node class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Our LinkedList class
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+
+  def get_head_node(self):
+    return self.head_node
+
+  # Add your insert_beginning and stringify_list methods below:
+
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+
+# Test your code by uncommenting the statements below - did your list print to the terminal?
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+print(ll.stringify_list())
+
+
+#extra oefening met verwijderen van Node
+
+# We'll be using our Node class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Our LinkedList class
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+
+  def get_head_node(self):
+    return self.head_node
+
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+
+
+  # Define your remove_node method below:
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+
+
+from node import Node
+
+class Stack:
+  def __init__(self):
+    self.top_item = None
+
+  # Define your push() and pop() methods below:
+
+  def push(self,value):
+    item = Node(value)
+    item.set_next_node(self.top_item)
+    self.top_item = item
+
+  def pop(self):
+    item_to_remove  = self.top_item
+    top_item = item_to_remove
+    return item_to_remove.value
+
+  def peek(self):
+    return self.top_item.get_value()
+
+
+
+from node import Node
+
+class Stack:
+  def __init__(self, limit=1000):
+    self.size = 0
+    self.limit = limit
+    self.top_item = None
+
+
+  def push(self, value):
+    item = Node(value)
+    item.set_next_node(self.top_item)
+    self.top_item = item
+
+  def pop(self):
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      # Decrement the stack size here:
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("stack is empty")
+
+  def peek(self):
+    if self.size > 0:
+      return self.top_item.get_value()
+    else:
+      print("stack is empty")
