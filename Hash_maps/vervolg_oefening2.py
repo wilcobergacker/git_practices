23c412c18dd075dd8c0e9a17ba3f0cb71b3ccc42
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-19 18:07:20 +0100

    next step added

--- Hash_maps/vervolg_oefening2.py
+++ Hash_maps/vervolg_oefening2.py
@@ -1,21 +1,20 @@
 class HashMap:
-  #Create an instance variable called .array, which is a list of size array_size. Make each element of .array equal to None
   def __init__(self, array_size):
     self.array_size = array_size
-
     self.array = [None for item in range(array_size)]
 
   def hash(self, key):
-    #.encode() is a string method that converts a string into its corresponding bytes, a list-like object with the numerical representation of each character in the string.
     key_bytes = key.encode()
-    #sums the numbers in key_bites
     hash_code = sum(key_bytes)
     return hash_code
 
   def compressor(self, hash_code):
-  #Take the modulus of the hash code by the map’s array_size in order to reduce the hash code to a possible index for the array
     return hash_code % self.array_size
 
+#controle of een key in de array leeg is, zo ja plaats hem.
+#If current_array_value already has contents, check if the saved key is different from the key we are currently processing. If the keys are the same, overwrite the array value.
+#
+#If the keys are different, we’re going to implement a way to find the next array index where our key should go. We'll get to handling different keys later.
   def assign(self, key, value):
     array_index = self.compressor(self.hash(key))
     current_array_value = self.array[array_index]
@@ -30,14 +29,16 @@ class HashMap:
 
     # current_array_value currently holds different key
     return
-
+#de retrieve functie is aangepast, om te checken of de key bestaat en of dezelfde key al aanwezig is.
   def retrieve(self, key):
-      #gelijk aan de assign functie, maar gesplitst
     array_index = self.compressor(self.hash(key))
-    return self.array[array_index]
+    possible_return_value = self.array[array_index]
 
+    if possible_return_value is None:
+        return None
 
-hash_map = HashMap(20)
-hash_map.assign("gneiss","metamorphic")
+    if possible_return_value[0] == key:
+        return possible_return_value[1]
 
-print(hash_map.retrieve("gneiss"))
+    # possible_return_value holds different key
+    return
