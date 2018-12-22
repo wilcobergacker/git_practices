cb9850fc8e7f75d6cf7afd27d640b01cf85d0c82
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-22 15:42:42 +0100

    added traverse def

--- Trees/classTreeNode.py
+++ Trees/classTreeNode.py
@@ -13,6 +13,14 @@ class TreeNode:
     self.children = [child for child in self.children
                      if child is not child_node]
 
+  def traverse(self):
+    print("Traversing...")
+    nodes_to_visit = [self]
+    while len(nodes_to_visit) != 0:
+      current_node = nodes_to_visit.pop()
+      print(current_node.value)
+      nodes_to_visit += current_node.children
+      
 root = TreeNode("I am Root")
 child = TreeNode("A wee sappling")
 bad_seed = TreeNode("Root Rot!")
