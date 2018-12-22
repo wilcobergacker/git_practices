3b3e3fa0505a038dd044e0e65860b513151527ad
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-22 21:59:22 +0100

    added get_smaller_child

--- Heap_maps/remove_from_heaplist.py
+++ Heap_maps/remove_from_heaplist.py
@@ -14,9 +14,12 @@ class MinHeap:
   def right_child_idx(self, idx):
     return idx * 2 + 1
 
+  # NEW HELPER!
+  def child_present(self, idx):
+    return self.left_child_idx(idx) <= self.count
+
   # END OF HEAP HELPER METHODS
 
-  # define .retrieve_min() below...
   def retrieve_min(self):
     if self.count == 0:
       print("No items in heap")
@@ -25,9 +28,10 @@ class MinHeap:
     min = self.heap_list[1]
     print("Removing: {0} from {1}".format(min, self.heap_list))
     self.heap_list[1] = self.heap_list[self.count]
-    self.heap_list.pop()
     self.count -= 1
+    self.heap_list.pop()
     print("Last element moved to first: {0}".format(self.heap_list))
+    self.heapify_down()
     return min
 
   def add(self, element):
@@ -35,6 +39,20 @@ class MinHeap:
     print("Adding: {0} to {1}".format(element, self.heap_list))
     self.heap_list.append(element)
     self.heapify_up()
+
+
+  def heapify_down(self):
+    idx = 1
+    while self.child_present(idx):
+      print("Heapifying down!")
+      smaller_child_idx = self.get_smaller_child_idx(idx)
+      child = self.heap_list[smaller_child_idx]
+      parent = self.heap_list[idx]
+      if parent > child:
+        self.heap_list[idx] = child
+        self.heap_list[smaller_child_idx] = parent
+      idx = smaller_child_idx
+    print("HEAP RESTORED! {0}".format(self.heap_list))
 
   def get_smaller_child_idx(self, idx):
     if self.right_child_idx(idx) > self.count:
