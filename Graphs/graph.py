ee0b8442c7bb00ff829df2d283369fb0e60ca77b
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-23 16:13:46 +0100

    added third  part of graph

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -10,3 +10,18 @@ class Graph:
     self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
     if not self.directed:
       self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+
+  def find_path(self, start_vertex, end_vertex):
+    print("Searching from {0} tp {1}".format(start_vertex, end_vertex))
+    start = [start_vertex]
+    while start:
+      current_vertex = start.pop(0)
+      print("Current Vertex: " + current_vertex)
+            #START CODE HERE
+      if current_vertex == end_vertex:
+        return True
+      vertex = self.graph_dict[current_vertex]
+      next_vertices = vertex.get_edges()
+      start += next_vertices
+    return False
