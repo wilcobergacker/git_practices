c2111560cfced8716f504c54ebc3a3256613af8b
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-24 13:44:56 +0100

    recursion added

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -16,3 +16,15 @@ def sum_to_one(n):
   return result, call_stack
 
 sum_to_one(4)
+
+
+
+# Define sum_to_one() below...
+def sum_to_one(n):
+  if n == 1:
+    return n
+  print("Recursing with input: {0}".format(n))
+  return n + sum_to_one(n-1)
+
+# uncomment when you're ready to test
+print(sum_to_one(7))
