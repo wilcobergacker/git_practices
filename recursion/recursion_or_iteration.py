94f54f0ae5d27e820b59e62255ba8f992e6f71b1
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-25 23:05:52 +0100

    recursion added

--- recursion/recursion_or_iteration.py
+++ recursion/recursion_or_iteration.py
@@ -9,3 +9,20 @@ def factorial(n):
 print(factorial(3) == 6)
 print(factorial(0) == 1)
 print(factorial(5) == 120)
+
+
+def fibonacci(n):
+  fibs = [0,1]
+  if n <= len(fibs) - 1:
+    return fibs[n]
+  else:
+    while n > len(fibs) - 1:
+      next_fib = fibs[-1] + fibs[-2]
+      fibs.append(next_fib)
+    return fibs[n]
+
+
+# test cases
+print(fibonacci(3) == 2)
+print(fibonacci(7) == 13)
+print(fibonacci(0) == 0)
