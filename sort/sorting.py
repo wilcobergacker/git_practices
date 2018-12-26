80a047187ee624d502a08f1dddc49098c162d166
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-26 16:21:10 +0100

    add sort

--- sort/sorting.py
+++ sort/sorting.py
@@ -6,7 +6,14 @@ def swap(arr, index_1, index_2):
   arr[index_1] = arr[index_2]
   arr[index_2] = temp
 
+# define bubble_sort():
+def bubble_sort(arr):
+  for i in range(len(arr)-1):
+    if arr[i] > arr[i + 1]:
+      swap(arr, i, (i + 1))
 
+##### test statements
 
-#swap(nums, 3, 5)
-print(nums)
+print("Pre-Sort: {0}".format(nums))
+bubble_sort(nums)
+print("Post-Sort: {0}".format(nums))
