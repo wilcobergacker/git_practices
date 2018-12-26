8f5b55642be06c42fff1d3ba1fadc1c5ea54abe6
Wilco Bergacker <wilcobergacker@gmail.com>
2018-12-26 16:13:10 +0100

    add sort

---
+++ sort/sorting.py
@@ -0,0 +1,12 @@
+nums = [5, 2, 9, 1, 5, 6]
+
+# Define swap() below:
+def swap(arr, index_1, index_2):
+  temp = arr[index_1]
+  arr[index_1] = arr[index_2]
+  arr[index_2] = temp
+
+
+
+#swap(nums, 3, 5)
+print(nums)
